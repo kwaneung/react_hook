b4c90fe271861d9886c998014e101ccaa6b9d0c5
kwaneung <kwaneung.kim@outlook.com>
2023-03-21 14:41:34 +0900

    useReducer 응용2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Counter from "./component/Counter";
 import {Fragment} from "react";
+import Info from "./component/Info";
 
 const App = () => {
   return (
       <Fragment>
           <Counter/>
+          <br/>
+          <Info/>
       </Fragment>
   );
 };
