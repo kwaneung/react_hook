19ba07deaacedb91ab733582e25222bae7039cf5
kwaneung <kwaneung.kim@outlook.com>
2023-03-21 15:09:24 +0900

    context 예제

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Counter from "./component/Counter";
 import {Fragment} from "react";
 import Info from "./component/Info";
+import ContextSample from "./component/ContextSample";
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
           <Counter/>
           <br/>
           <Info/>
+          <br/>
+          <ContextSample/>
       </Fragment>
   );
 };
