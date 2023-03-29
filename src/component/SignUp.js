7c795797ac33511e98ed07e58bc2a604717a56af
kwaneung <kwaneung.kim@outlook.com>
2023-03-29 17:44:54 +0900

    useRef 예제

--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -1,17 +1,22 @@
-import React, { useState, useCallback } from 'react';
+import React, {useState, useRef, useCallback} from 'react';
 
 function SignUp() {
-    const [mail, setMail] = useState('');
+    const mail = useRef('');
     const [result, setResult] = useState('');
 
     console.log('render');
 
+    const onClick = useCallback(() => {
+        setResult(mail.current.value);
+        mail.current.focus();
+    }, []); // 컴포넌트가 처음 렌더링 될 때만 함수 생성
+
     return (
         <div>
             <span>이메일 주소</span><br/>
-            <input value={mail} onChange={(e) => setMail(e.target.value)} /><br/>
+            <input ref={mail} /><br/>
             <span>결과: {result}</span><br/>
-            <button onClick={() => setResult(mail)}>회원가입</button>
+            <button onClick={onClick}>회원가입</button>
         </div>
     );
 }
