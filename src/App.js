ac78f52e583c71bd0e3b31e0eeb753e2b6443fd1
kwaneung <kwaneung.kim@outlook.com>
2023-03-29 15:21:33 +0900

    useMemo, useCallback

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Counter from "./component/Counter";
 import {Fragment} from "react";
 import Info from "./component/Info";
 import ContextSample from "./component/ContextSample";
+import Average from "./component/Average";
 
 const App = () => {
   return (
@@ -12,6 +13,10 @@ const App = () => {
           <Info/>
           <br/>
           <ContextSample/>
+          <br/>
+          ==================
+          <br/>
+          <Average/>
       </Fragment>
   );
 };
